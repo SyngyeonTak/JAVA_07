963cf0860de17bf2b7031c8cad6049e751fb7aeb
SyngyeonTak <67184044+SyngyeonTak@users.noreply.github.com>
2020-08-10 19:35:01 +0900

    NestedFor

---
+++ src/java_07/NestedFor.java
@@ -0,0 +1,12 @@
+package java_07;
+
+public class NestedFor {
+	public static void main(String[] args) {
+		for(int i = 1; i<10; i++) {
+			for(int j = 1; j<10; j++) {
+				System.out.printf("%d*%d=%d\n", i, j, i*j);
+			}
+			System.out.println();
+		}
+	}
+}
